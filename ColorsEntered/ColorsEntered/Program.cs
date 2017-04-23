ffd43417a132fe2b30c161affd19c92fa3b28034
vladas1987 <petras18@gmail.com>
2017-04-23 18:47:02 -0400

    C# ColorEntered

---
+++ ColorsEntered/ColorsEntered/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColorsEntered
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your favorite colors below :");
+            char[] delimeters = { ',', ':', ';', ' ' };
+            string[] colors = Console.ReadLine().Split(delimeters);
+            Console.WriteLine();
+            Console.WriteLine("You have {0} favorite colors:", colors.Length);
+            Console.WriteLine("Press any key to exit: ");
+            Console.ReadKey();
+        }
+    }
+}
